dd2c7e6f670c91ff8674e34e6db0bceec4b033b9
Yassine <99072435@mydavinci.nl>
2023-03-15 11:25:41 +0100

    15-3-23

--- Javascript/Inlog page/script.js
+++ Javascript/Inlog page/script.js
@@ -1,7 +1,14 @@
-loggedIn = ["hans","klaas"];
+lijst = [""];
+
+function getIndexOfElementByName(lijst,naam){
+    if (naam in loggedIn);
+    return -1
+}
+
 
 function handlelogin(){
-    console.log(loggedIn)
+    console.log(lijst)
+    console.log(Date())
 
     let naam = document.getElementById('inputNaam').value;// stap 1 ophalen input
     if (naam.length <= 0){
@@ -9,13 +16,15 @@ function handlelogin(){
         return
     }
     console.log("U hebt op de button geklikt");
-    if (loggedIn.includes(naam)){ // stap 2 bepalen of persoon in de array zit
-        loggedIn.splice(loggedIn.indexOf(naam),1)// stap 3 verwijder persoon uit array
+
+    let posittie = getIndexOfElementByName('InputNaam');
+    if (posittie > -1){ // stap 2 bepalen of persoon in de array zit
+        lijst.splice(lijst.indexOf(naam),1)// stap 3 verwijder persoon uit array
         console.log("uw bent uitgelogd")
         document.getElementById("melding").innerText = "uw bent uitgelogd: " + naam;
         document.getElementById('inputNaam').value = '';
     } else{// niet ingelogd dan toevoegen aan array
-        loggedIn.push(naam);
+        loggedIn.push({"naam": naam , "momment":  new Date()});
         document.getElementById("melding").innerText = "uw bent in gelogd: " + naam;
         console.log("uw bent ingelogd " + naam);
     }
--- Javascript/horeca app/horeca-app.js
+++ Javascript/horeca app/horeca-app.js
@@ -1,4 +1,17 @@
-const fris = 3
-const wijn = 7
-const bier = 4
-
+// variable met de prijzen van het drinken
+const fris = 3.0;
+const wijn = 7.0;
+const bier = 4.0;
+//lijst om aan toegvoegen
+const lijst = []
+let aantal = 0
+// functie om drankje het te toevoegen
+function myFunction() {
+if (option.value =="fris"){
+document.getElementById("fris").innerHTML = "Fris : " + fris + " euro";
+lijst.push({ name: "Fris", price: fris });
+    }
+if (option.value =="wijn"){
+    document.getElementById("fris").innerHTML = "Wijn : " + wijn + " euro";
+    }
+}
