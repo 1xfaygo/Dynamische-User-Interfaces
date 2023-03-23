cfc7c5e6a5e10a39f92fdb1a31b1918f77980ed2
Yassine <99072435@mydavinci.nl>
2023-03-23 10:21:47 +0100

    '10:20'

--- Javascript/horeca app/horeca-app.js
+++ Javascript/horeca app/horeca-app.js
@@ -1,17 +1,42 @@
 // variable met de prijzen van het drinken
-const fris = 3.0;
-const wijn = 7.0;
-const bier = 4.0;
+const fris_prijs = 2.0;
+const wijn_prijs = 10.0;
+const bier_prijs = 4.0;
 //lijst om aan toegvoegen
-const lijst = []
-let aantal = 0
-// functie om drankje het te toevoegen
-function myFunction() {
-if (option.value =="fris"){
-document.getElementById("fris").innerHTML = "Fris : " + fris + " euro";
-lijst.push({ name: "Fris", price: fris });
-    }
-if (option.value =="wijn"){
-    document.getElementById("fris").innerHTML = "Wijn : " + wijn + " euro";
-    }
+const lijst = {
+    fris: 0,
+    bier: 0,
+    wijn: 0
 }
+
+while (true){
+let drankje = prompt("Welk drankje wilt u toevoegen aan uw bestelling? fris, bier of wijn of type stop om te stoppen met bestellen");
+
+if (drankje == "fris" || drankje == "bier" || drankje == "wijn"){
+while(true){
+let aantal_drank = prompt(" hoveel "+ drankje + " wil je: ?");
+    lijst[drankje] = aantal_drank
+    break;
+}
+}
+else if (drankje == "stop"){
+    break;
+}
+
+else{
+    alert("ken ik niet");
+}
+}
+
+
+totaal= lijst.fris * fris_prijs + lijst.bier * bier_prijs + lijst.wijn * wijn_prijs;
+
+aantal_fris =("aantal Fris:" + lijst.fris);
+aantal_bier =("aantal Bier:" + lijst.bier);
+aantal_wijn =("aantal Wijn:" + lijst.wijn);
+let totaal_prijs = "totaal:" + Math.round(lijst.fris * fris_prijs + lijst.bier * bier_prijs + lijst.wijn * wijn_prijs) + "euro";
+
+document.getElementById("aantal_fris").innerHTML = aantal_fris
+document.getElementById("aantal_bier").innerHTML = aantal_bier
+document.getElementById("aantal_wijn").innerHTML = aantal_wijn
+document.getElementById("totaal_prijs").innerHTML = totaal_prijs
