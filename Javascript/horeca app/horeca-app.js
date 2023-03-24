3b05e164b6e7be6c2b9ab113cda62a51c928fe93
Yassine <99072435@mydavinci.nl>
2023-03-24 10:18:40 +0100

    horeca app

--- Javascript/horeca app/horeca-app.js
+++ Javascript/horeca app/horeca-app.js
@@ -1,43 +1,46 @@
-// variable met de prijzen van het drinken
-const fris_prijs = 2.0;
-const wijn_prijs = 10.0;
-const bier_prijs = 4.0;
 //lijst om aan toegvoegen
 const lijst = {
-    fris: 0,
-    bier: 0,
-    wijn: 0
-}
-function bestelling(){
-while (true){
-let drankje = prompt("Welk drankje wilt u toevoegen aan uw bestelling? fris, bier of wijn of type stop om te stoppen met bestellen");
 
-if (drankje == "fris" || drankje == "bier" || drankje == "wijn"){
-while(true){
-let aantal_drank = parseInt(prompt(" hoveel "+ drankje + " wil je: ?"));
-    lijst[drankje] = aantal_drank
-    break;
 }
+const prijs = {
+    fris: 2,
+    bier: 4,
+    wijn: 10,
+    cola: 3
 }
-else if (drankje == "stop"){
-    break;
+function bestelling() {
+    while (true) {
+        let drankje = prompt("Welk drankje wilt u toevoegen aan uw bestelling?");
+
+        if (drankje in prijs) {
+            while (true) {
+                let aantal_drank = parseInt(prompt(" hoveel " + drankje + " wil je: ?"));
+
+                if (drankje in lijst) {
+                    lijst[drankje] += aantal_drank;
+                } else {
+                    lijst[drankje] = aantal_drank;
+                }
+
+                break;
+            }
+        } else if (drankje == "stop") {
+            break;
+        } else {
+            alert("ken ik niet");
+        }
+    }
 }
 
-else{
-    alert("ken ik niet");
-}
-}
-}
 bestelling();
 
-totaal= lijst.fris * fris_prijs + lijst.bier * bier_prijs + lijst.wijn * wijn_prijs;
+let totaalbedrag = 0;
 
-aantal_fris =("aantal Fris:" + lijst.fris);
-aantal_bier =("aantal Bier:" + lijst.bier);
-aantal_wijn =("aantal Wijn:" + lijst.wijn);
-let totaal_prijs = "totaal:" + Math.round(lijst.fris * fris_prijs + lijst.bier * bier_prijs + lijst.wijn * wijn_prijs) + "euro";
-
-document.getElementById("aantal_fris").innerHTML = aantal_fris
-document.getElementById("aantal_bier").innerHTML = aantal_bier
-document.getElementById("aantal_wijn").innerHTML = aantal_wijn
+for (p in lijst) {
+    console.log("prijs: " + prijs[p] + " euro")
+    console.log("aantal " + lijst[p])
+    totaalbedrag += prijs[p] * lijst[p]
+}
+console.log(totaalbedrag)
+var totaal_prijs = "totaal: " + Math.round(totaalbedrag) + " euro";
 document.getElementById("totaal_prijs").innerHTML = totaal_prijs
