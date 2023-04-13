4c34f7440c22ed3b42c80a88685bff2f4e189853
Yassine <99072435@mydavinci.nl>
2023-04-13 10:16:24 +0200

    'button1'

---
+++ Javascript/Buttons 1.0/buttons-template/buttons.js
@@ -0,0 +1,21 @@
+function changeimage(){
+    var img = document.getElementById('0');
+    img.src = 'images/bg1.jpg'
+
+    var img2 =document.getElementById('000');
+    img2.src = 'images/1.jpg'
+}
+function changeimage2(){
+    var img = document.getElementById('0');
+    img.src = 'images/bg2.jpg'
+
+    var img2 =document.getElementById('000');
+    img2.src = 'images/2.jpg'
+}
+function changeimage3(){
+    var img = document.getElementById('0');
+    img.src = 'images/bg3.jpg'
+
+    var img2 =document.getElementById('000');
+    img2.src = 'images/3.jpg'
+}
